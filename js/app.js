b73403d3ef8fea369d543ab797f1596a354d7f4a
Catalina Sabogal <csabogalortiz@gmail.com>
2022-11-03 11:57:35 +0100

    Para Juli

--- js/app.js
+++ js/app.js
@@ -49,8 +49,8 @@ const app = {
 
     setDimensions() {
         this.canvasSize = {
-            w: 300,
-            h: 700,
+            w: 500,
+            h: 800,
         }
         document.querySelector('#myCanvas').setAttribute('height', this.canvasSize.h)
         document.querySelector('#myCanvas').setAttribute('width', this.canvasSize.w)
